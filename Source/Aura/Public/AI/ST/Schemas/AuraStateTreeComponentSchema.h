0a50bbe6846c6b83a05b0c3c41808f84aef9c809
Fernando Wals Rodriguez <fernando@signalspace.ca>
2024-12-16 20:53:02 +0100

    Added attribute menu skeleton and state tree module

---
+++ Source/Aura/Public/AI/ST/Schemas/AuraStateTreeComponentSchema.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/StateTreeComponentSchema.h"
+#include "AuraStateTreeComponentSchema.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UAuraStateTreeComponentSchema : public UStateTreeComponentSchema
+{
+	GENERATED_BODY()
+	
+};
