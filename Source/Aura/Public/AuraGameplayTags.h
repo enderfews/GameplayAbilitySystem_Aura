0b465749a4adb18e902654f14b6f6aad1ac6e810
Fernando Wals Rodriguez <fernando@signalspace.ca>
2024-12-17 20:32:07 +0100

    Aura gameplay tags and asset manager and attribute menu implementations

---
+++ Source/Aura/Public/AuraGameplayTags.h
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
+/**
+ * AuraGameplayTags
+ * 
+ * Singleton containing native gameplay tags
+ */
+
+struct FAuraGameplayTags
+{
+public:
+	static const FAuraGameplayTags& Get(){return GameplayTags;}
+	static void InitializeGameplayTags();
+
+	FGameplayTag Attributes_Secondary_Armor;
+protected:
+
+private:
+
+	static FAuraGameplayTags GameplayTags;
+};
---
+++ enc_temp_folder/148d42e98a87555414962662a316b16e/AuraGameplayTags.h
@@ -0,0 +1,23 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
+/**
+ * AuraGameplayTags
+ * 
+ * Singleton containing native gameplay tags
+ */
+
+struct FAuraGameplayTags
+{
+public:
+	static const FAuraGameplayTags& Get(){return GameplayTags;}
+	static void InitializeGameplayTags();
+protected:
+
+private:
+
+	static FAuraGameplayTags GameplayTags;
+};
